9cb1371bb3ca78032939c84bf27b2e6b972ccc56
Fayaz Laghari <lagharifayazali3@gmail.com>
2024-06-01 18:35:01 +0500

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
  return (
-  <div className="App">
-   <h1 className="text-cyan-400">hello world </h1>
-  </div>
+  <>
+   <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/forgot-password" element={<ForgotPassword />} />
+     <Route path="/offers" element={<Offers />} />
+     <Route path="/profile" element={<Profile />} />
+     <Route path="/sign-in" element={<SignIn />} />
+     <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+   </BrowserRouter>
+  </>
  );
 }
 
